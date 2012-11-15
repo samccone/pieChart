f5759bae8ec13375c81fbaab3b62d90c20f8e6ed
Sam Saccone <samccone@gmail.com>
2012-11-14 21:04:38 -0500

    comments are dandy for sure

--- pieChart.js
+++ pieChart.js
@@ -11,19 +11,19 @@
   } else {
     window.pieChart = function(options) {
 
-      var pixelDensity  = window.devicePixelRatio || 1;
+      var pixelDensity  = window.devicePixelRatio || 1; //grab the pixel density for retina goodness :)
 
-      var elm           = document.createElement('canvas'),
-          ctx           = elm.getContext('2d'),
-          radius        = (options.radius * pixelDensity) || (100 * pixelDensity),
-          lineWidth     = (pixelDensity * options.stroke) || (20 * pixelDensity),
-          startAngle    = 0 * Math.PI/180,
-          endAngle      = 360 * Math.PI/180,
-          registration  = radius + lineWidth/2
-          fillEndAngle  = ((options.fillPercent / 100 * 360) - 90) * Math.PI/180,
-          complete      = options.fillPercent == 100,
-          antiAliaisingClippingConts = 1,
-          drawOptions   = {
+      var elm                         = document.createElement('canvas'),
+          ctx                         = elm.getContext('2d'),
+          radius                      = (options.radius * pixelDensity) || (100 * pixelDensity), //set some defaults
+          lineWidth                   = (pixelDensity * options.stroke) || (20 * pixelDensity), // defaults
+          startAngle                  = 0 * Math.PI/180,
+          endAngle                    = 360 * Math.PI/180,
+          registration                = radius + lineWidth/2
+          fillEndAngle                = ((options.fillPercent / 100 * 360) - 90) * Math.PI/180,
+          complete                    = options.fillPercent >= 100, //make check to make sure if it is over 100% and handle it
+          antiAliaisingClippingConts  = 1,
+          drawOptions                 = {
                             ctx: ctx,
                             registration: registration,
                             radius: radius,
@@ -48,6 +48,9 @@
 
       drawArc(drawOptions); // draws the background
 
+      /**
+      * recalculate values for the fil stroke
+      **/
       drawOptions.startAngle  = complete ? startAngle : 270 * Math.PI/180;
       drawOptions.endAngle    = complete ? endAngle : fillEndAngle;
       drawOptions.clockwise   = 0;
@@ -56,6 +59,9 @@
       drawOptions.registration= drawOptions.registration;
       drawArc(drawOptions); // draws the filled %
 
+      /**
+      * helper function to do the drawing work
+      **/
       function drawArc(args) {
         args.ctx.strokeStyle  = args.strokeStyle;
         args.ctx.lineWidth    = args.lineWidth || 1;
@@ -65,8 +71,7 @@
         args.ctx.closePath();
       }
 
-
-      if(options.container && options.container.appendChild) {
+      if(options.container && options.container.appendChild) { //if a container elm was passed then add it to it
         options.container.appendChild(elm);
       } else {
         document.body.appendChild(elm);
